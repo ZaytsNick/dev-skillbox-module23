a7fd66b3c7ad59ffd6fde1ce8d44596e3b7b8abd
zayts_home <10kcin01@gmail.com>
2024-03-05 23:57:43 +0500

    Task#1 complete

--- Task#1/main1.cpp
+++ Task#1/main1.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
-#define DAY_1 "MONDEY"
+#define DAY_0 "no such day"
+#define DAY_1 "MONDAY"
 #define DAY_2 "TUESDAY"
 #define DAY_3 "WEDNESDAY"
 #define DAY_4 "THURSDAY"
@@ -8,37 +9,42 @@
 #define DAY_6 "SATURDAY"
 #define DAY_7 "SUNDAY"
 
+// #define DAY_OF_THE_WEEK(type, a, day) ({ decltype(a) type tmp=a; DAY##_+tmp; })
+// #define DAY_OF_THE_WEEK(a) std::cout<<DAY##_## a<<std::endl;
+// #define MAX(a, b) ({int _a = (a), _b = (b); _a > _b ? _a : _b; })
+//  #define DAY_OF_THE_WEEK(a) int _a=(a);std::cout<<DAY##_+_a;
+#define WRITE(num) std::cout << DAY##_##num << std::endl;
 int main()
 {
-    int number(1);
-    while (number > 0 && number < 8)
+    int number;
+    while (number != 0)
     {
         std::cin >> number;
         switch (number)
         {
         case 1:
-            std::cout << DAY_1 << std::endl;
+            WRITE(1);
             break;
         case 2:
-            std::cout << DAY_2 << std::endl;
+            WRITE(2);
             break;
         case 3:
-            std::cout << DAY_3 << std::endl;
+            WRITE(3);
             break;
         case 4:
-            std::cout << DAY_4 << std::endl;
+            WRITE(4);
             break;
         case 5:
-            std::cout << DAY_5 << std::endl;
+            WRITE(5);
             break;
         case 6:
-            std::cout << DAY_6 << std::endl;
+            WRITE(6);
             break;
         case 7:
-            std::cout << DAY_7 << std::endl;
+            WRITE(7);
             break;
-
         default:
+            WRITE(0);
             break;
         }
     }

da9c090367348b232c9a1c5783ebe578b293c4d9
zayts_home <10kcin01@gmail.com>
2024-03-10 21:31:49 +0500

    task#2

--- Task#2/main2.cpp
+++ Task#2/main2.cpp
@@ -1,6 +1,6 @@
 // SPRING, SUMMER, AUTUMN или WINTER
 #include <iostream>
-#define WINTER "Hello winter world"
+#define WINTER "Hello world"
 // int main()
 // {
 //     std::cout << "Hello world"  << std::endl;
@@ -8,24 +8,24 @@
 #if defined(SPRING)
 int main()
 {
-    std::cout << #SEASON << std::endl;
+    std::cout << SPRING << std::endl;
 }
 #endif
 #if defined(SUMMER)
 int main()
 {
-    std::cout << #SEASON << std::endl;
+    std::cout << SUMMER << std::endl;
 }
 #endif
 #if defined(AUTUMN)
 int main()
 {
-    std::cout << #SEASON << std::endl;
+    std::cout << AUTUMN << std::endl;
 }
 #endif
 #if defined(WINTER)
 int main()
 {
-    std::cout << WINTER<< std::endl;
+    std::cout << WINTER << std::endl;
 }
 #endif
